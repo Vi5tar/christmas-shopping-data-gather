66ee8ebfdbe942e912013ad582ad876aa6448a54
Travis Swiers <tswiers85@gmail.com>
2020-12-06 09:17:06 -0700

    feat: fake data to work with

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Wish;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        
+        User::factory()
+            ->count(10)
+            ->has(Wish::factory()->count(rand(1, 6)))
+            ->create();
     }
 }
